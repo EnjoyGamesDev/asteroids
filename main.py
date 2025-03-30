3971ab902d2c2ae9c83e22928d7f316cc14d25c0
EnjoyGamesDev <46535302+EnjoyGamesDev@users.noreply.github.com>
2025-03-29 21:36:20 -0600

    added player rotation

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
         
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         
         # Limit the framerate to 60
