375da641e011b1f828b61c83e50a384bb0a18e42
EnjoyGamesDev <46535302+EnjoyGamesDev@users.noreply.github.com>
2025-03-30 20:22:10 -0600

    set pygame window title to "Asteroids"

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    screen = pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =  pygame.time.Clock()
     dt = 0
