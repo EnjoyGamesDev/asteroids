f6de0fe0e788c84321bbfd2ffa177371a34bf2ba
EnjoyGamesDev <46535302+EnjoyGamesDev@users.noreply.github.com>
2025-03-30 21:02:42 -0600

    display points and fps on-screen

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =  pygame.time.Clock()
     dt = 0
+    points = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -28,6 +29,12 @@ def main():
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
+    
+    # Function to draw the points
+    text_font = pygame.font.SysFont(None, 30)
+    def draw_text(text, font, text_col, x, y):
+        img = font.render(text, True, text_col)
+        screen.blit(img, (x, y))  
 
     while True:
         for event in pygame.event.get():
@@ -46,6 +53,7 @@ def main():
         # then delete that asteroid and the bullet        
             for shot in shots:
                 if shot.collision(asteroid):
+                    points += 1
                     shot.kill()
                     asteroid.split()
                         
@@ -54,12 +62,13 @@ def main():
         for obj in drawable:
             obj.draw(screen)
         
+        draw_text(f"Points: {points}", text_font, "white", 15, 15)
+        fps = clock.get_fps()
+        draw_text(f"FPS: {fps:.2f}", text_font, "white", 15, 50)
         pygame.display.flip()
         
         # Limit the framerate to 60
         dt = clock.tick(60) / 1000
-        
-    
 
 if __name__ == "__main__":
     main()
